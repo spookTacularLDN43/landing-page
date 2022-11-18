53fce294cd2731641032d4e529c92ccbadf3b5d5
spookTacularLDN43 <117355850+spookTacularLDN43@users.noreply.github.com>
2022-11-18 14:58:47 +0100

    mobile-menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,27 @@
-console.log('hello world'); 
+const menuButton = document.querySelector('#menuButton');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+const fadeElements = document.querySelectorAll('.has-fade');
+const body = document.querySelector('.body');
+
+menuButton.addEventListener('click', function(){
+
+    if(header.classList.contains('open')){
+        //body.classList.remove('noscroll');
+        header.classList.remove('open');
+        fadeElements.forEach(function(element){
+            element.classList.remove('fade-in');
+            element.classList.add('fade-out'); 
+        })
+        
+        
+    }
+    else{
+    //body.classList.add('noscroll'); 
+    header.classList.add('open');
+    fadeElements.forEach(function(element){
+    element.classList.add('fade-in');
+    element.classList.remove('fade-out');
+    })
+    }
+});
